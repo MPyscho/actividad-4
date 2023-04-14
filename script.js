84cd2039a59a57b58f44db902817649952e5e419
MPyscho <123724212+MPyscho@users.noreply.github.com>
2023-04-14 09:12:10 +0200

    todos los if else

--- script.js
+++ script.js
@@ -3,3 +3,16 @@ alert('Hola, se le pedirá un número');
 primerNumero = prompt("introduzca un numero");
 let segundoNumero;
 segundoNumero= prompt('introduzca un numero')
+if (primerNumero>segundoNumero){
+    alert(primerNumero 'es mayor que' segundoNumero)    
+} else {
+    alert(primerNumero 'es menor que' segundoNumero)
+}
+if(primerNumero==segundoNumero) {
+    alert(primerNumero 'es igual que' segundoNumero)
+}
+if(segundoNumero %2==1){
+   alert(segundoNumero 'es impar') 
+} else {
+    alert(segundoNumero 'es par')
+}
